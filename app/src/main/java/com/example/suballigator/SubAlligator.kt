77a1a5d59fd5170d173b68b8e47a28aba1570d7c
TheHangel <105165180+TheHangel@users.noreply.github.com>
2023-03-13 16:30:29 +0100

    activité view

---
+++ app/src/main/java/com/example/suballigator/SubAlligator.kt
@@ -0,0 +1,11 @@
+package com.example.suballigator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SubAlligator : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub_alligator)
+    }
+}
