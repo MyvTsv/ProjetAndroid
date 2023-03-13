3e0b2ee38828979d1ea94063244aa8473be94a4d
TheHangel <105165180+TheHangel@users.noreply.github.com>
2023-03-13 16:37:33 +0100

    tout beau tout propre

--- app/src/main/java/com/example/suballigator/entitee/Formation.kt
+++ app/src/main/java/com/example/suballigator/entitee/Formation.kt
@@ -1,6 +1,5 @@
 package com.example.suballigator.entitee
 
-import androidx.room.Delete
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
--- app/src/main/java/com/example/suballigator/entitee/Level.kt
+++ app/src/main/java/com/example/suballigator/entitee/Level.kt
@@ -1,7 +1,6 @@
 package com.example.suballigator.entitee
 
 import androidx.room.Entity
-import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "Level")
