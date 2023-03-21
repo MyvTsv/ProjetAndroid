e4f9a138d24e8458dd9f00247d86624896f2e1b6
MyuTsu <gaetan.langlois31@gmail.com>
2023-03-21 21:02:39 +0100

    Correction BottomBar

--- app/src/main/java/com/example/suballigator/BottomMenuItem.kt
+++ app/src/main/java/com/example/suballigator/BottomMenuItem.kt
@@ -6,9 +6,9 @@ import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class BottomMenuItem(val route: String, val title: String, val icon: ImageVector) {
 
-    object formationScreen: BottomMenuItem("FormationScreen", "FormationScreen", Icons.Default.Person)
-    object etudiantScreen: BottomMenuItem("EtudiantScreen", "EtudiantScreen", Icons.Default.Person)
-    object seanceScreen: BottomMenuItem("SeanceScreen", "SeanceScreen", Icons.Default.Person)
-    object profilScreen: BottomMenuItem("ProfilScreen", "ProfilScreen", Icons.Default.Person)
+    object formationScreen: BottomMenuItem("FormationScreen", "Formation", Icons.Default.Person)
+    object etudiantScreen: BottomMenuItem("EtudiantScreen", "Etudiant", Icons.Default.Person)
+    object seanceScreen: BottomMenuItem("SeanceScreen", "Seance", Icons.Default.Person)
+    object profilScreen: BottomMenuItem("ProfilScreen", "Profil", Icons.Default.Person)
 
 }
