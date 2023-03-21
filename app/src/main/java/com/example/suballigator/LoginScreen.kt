4fceb5ff6c60a9e54e0f666f21ff64ad61e72bd0
TheHangel <105165180+TheHangel@users.noreply.github.com>
2023-03-21 13:33:50 +0100

    verification connexion fonctionne

--- app/src/main/java/com/example/suballigator/LoginScreen.kt
+++ app/src/main/java/com/example/suballigator/LoginScreen.kt
@@ -1,12 +1,18 @@
 package com.example.suballigator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.PasswordTransformationMethod
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import com.example.suballigator.entity.Initiator
 import com.example.suballigator.entity.Participant
 import kotlinx.coroutines.runBlocking
+import org.w3c.dom.Text
+import kotlin.math.log
 
 class LoginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,26 +23,60 @@ class LoginScreen : AppCompatActivity() {
         val passwordInput = findViewById<EditText>(R.id.passwordInput)
         val loginButton = findViewById<Button>(R.id.loginButton)
 
+        passwordInput.transformationMethod = PasswordTransformationMethod.getInstance()
+
         loginButton.setOnClickListener {
-            val email = emailInput.text.toString()
-            val password = passwordInput.text.toString()
 
-            runBlocking { insertDataAPI(application) }
+            loginButton.isEnabled = false
+            it.isEnabled = false
 
-            val initiators: List<Initiator>? = getInitiator(application)
+            if(emailInput.text.toString().isEmpty() || passwordInput.text.toString().isEmpty()) {
 
-            val existe = initiatorExist(initiators!!, email, password)
+                val email = emailInput.text.toString()
+                val password = passwordInput.text.toString()
 
-            }
-        }
+                runBlocking { insertDataAPI(application) }
 
-        private fun initiatorExist(initiators : List<Initiator>, email:String, password:String):Boolean{
-            initiators?.forEach { initiator ->
-                if (email == initiator.email && password == initiator.password) {
-                    return true
+                val initiators: List<Initiator>? = getInitiator(application)
+
+                val existe = initiatorExist(initiators!!, email, password)
+
+                if (existe) {
+                    val nextPage = Intent(this, ListeDesFormationsAcitivty::class.java)
+                    startActivity(nextPage)
+                    finish()
+                } else {
+                    val builder = AlertDialog.Builder(this)
+                    builder.setTitle("Erreur")
+                    builder.setMessage("Email ou mot de passe incorrect")
+                    builder.setPositiveButton("OK") { dialog, which ->
+                        dialog.dismiss()
+                    }
+                    val dialog: AlertDialog = builder.create()
+                    dialog.show()
                 }
+            } else{
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Erreur")
+                builder.setMessage("Veuillez remplir tous les champs")
+                builder.setPositiveButton("OK") { dialog, which ->
+                    dialog.dismiss()
+                }
+                val dialog: AlertDialog = builder.create()
+                dialog.show()
             }
-            return false
+            loginButton.isEnabled = true
+            it.isEnabled = true
         }
+    }
+
+    private fun initiatorExist(initiators : List<Initiator>, email:String, password:String):Boolean{
+        initiators?.forEach { initiator ->
+            if (email == initiator.email && password == initiator.password) {
+                return true
+            }
+        }
+        return false
+    }
 
 }
